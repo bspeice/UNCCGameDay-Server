f92f830110944b6af51e840c257209bc8f9b9595
Bradlee Speice <bspeice.nc@gmail.com>
2013-10-09 21:37:12 -0400

    Add the initial RegisteredUser class, remove standard Django apps

--- uncc_gameday/gameday/models.py
+++ uncc_gameday/gameday/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class RegisteredUser(models.Model):
+
+	date_registered = models.DateTimeField()
+	first_name = models.CharField(max_length=64)
+	last_name = models.CharField(max_length=64)
+	section = models.CharField(max_length=8)
+	row = models.IntegerField()
