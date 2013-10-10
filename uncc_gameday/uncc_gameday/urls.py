74fd422dfc0acb183d443aaabe508d555ddc96b1
Bradlee Speice <bspeice.nc@gmail.com>
2013-10-09 21:17:49 -0400

    Add initial code for the server Django project
    Current dependencies are Django and Django REST Framework

---
+++ uncc_gameday/uncc_gameday/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'uncc_gameday.views.home', name='home'),
+    # url(r'^uncc_gameday/', include('uncc_gameday.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+	url(r'^gameday/', include('gameday.urls')),
+	url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
+)
